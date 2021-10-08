e04443f335abe841fa82b8182b1d79e621a59c24
seergiojm23 <sjimenezr@alumnos.unex.es>
2021-10-08 16:36:03 +0200

    HUBBNIU

--- Campeonato.java
+++ Campeonato.java
@@ -11,7 +11,7 @@ public class Campeonato
     private String nombre;
     private String competidores;
     private String eliminados;
-    
+    //ESOS CHAVALES
     
     /**
      * Constructor for objects of class Campeonato
