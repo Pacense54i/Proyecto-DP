a19401fe180002f6199a7e157fa0043cc5dd3c67
seergiojm23 <sjimenezr@alumnos.unex.es>
2021-10-14 22:02:14 +0200

    salen en naranja en lugar de verde pero porque está en modo lectura vaya

---
+++ InitTennisData.java
@@ -0,0 +1,74 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Clase que carga los datos con los que simular un campeonato de tenis
+ * 
+ * @author Equipo Docente DP 
+ * @version 1.0
+ */
+public class InitTennisData {
+
+    public InitTennisData(Campeonato campeonato) {
+	//Descomentar la carga de datos que se quiera probar
+        cargarDatos1(campeonato);//Produce la salida Salida1_EC1
+		//cargarDatos2(campeonato);/Produce la salida Salida2_EC1
+        
+    }
+    
+    private void cargarDatos1(Campeonato campeonato) {
+		//Instanciamos 8 zapatillas: (Los parámetros están en el orden: modelo, número, tipo valor)
+		 /*z1 = ("Air Zoom Vapor Pro", 41, "amortiguacion", 5);
+		 z2 = ("Court Vapor React", 41, "agarre", 5);
+		 z3 = ("Gel-Resolution 8 GS", 41, "agarre", 4);
+		 z4 = ("Vapor Lite", 41, "amortiguacion", 3);
+		 z5 = ("Court Lite", 41, "agarre", 3);
+		 z6 = ("UberSonic 4K", 41, "amortiguacion", 5.5);
+		 z7 = ("Zoom GP Turbo", 41, "amortiguacion", 6);
+		 z8 = ("CourtJam Bounce", 41, "agarre", 3.5);
+
+
+
+		//Instanciamos 8 tenistas: (Los parámetros están en el orden nombre, zapatillas, saque, resto, ranking, pais)
+		 t1 = ("Novak Djokovic", z1, 90, 80, 1, "Serbia");
+		 t2 = ("Ashleigh Barty", z3, 70, 80, 1, "Australia");
+		 t3 = ("Stefanos Tsitsipas", z7, 55, 48, 3, "Grecia");
+		 t4 = ("Karolina Pliskova", z5, 67, 68.5, 3, "Rep Checa");
+		 t5 = ("Elina Svitolina", z6, 55.5, 68.5, 4, "Ucrania");
+		 t6 = ("Alexander Zverev", z8, 59, 49.5, 4, "Alemania");
+		 t7 = ("Aryna Sabalenka", z2, 67.5, 79.5, 2, "Bielorrusia");
+		 t8 = ("Daniil Medvedev", z4, 79, 68, 2, "Rusia");
+		 
+		 //A continuación se inscribirán a los 8 tenistas en el campeonato
+    }
+	
+	private void cargarDatos2(Campeonato campeonato) {
+        //Instanciamos 8 zapatillas: (Los parámetros están en el orden: modelo, número, tipo valor)
+		z1 = ("Air Zoom Vapor Pro", 41, "amortiguacion", 5);
+		z2 = ("Court Vapor React", 41, "agarre", 5);
+		z3 = ("Gel-Resolution 8 GS", 41, "agarre", 4);
+		z4 = ("Vapor Lite", 41, "amortiguacion", 3);
+		z5 = ("Court Lite", 41, "agarre", 3);
+		z6 = ("UberSonic 4K", 41, "amortiguacion", 5.5);
+		z7 = ("Zoom GP Turbo", 41, "amortiguacion", 6);
+		z8 = ("CourtJam Bounce", 41, "agarre", 3.5);
+
+
+
+		//Instanciamos 8 tenistas: (Los parámetros están en el orden nombre, zapatillas, saque, resto, ranking, pais)
+		t1 = ("Novak Djokovic", z3, 90, 80, 1, "Serbia");
+		t2 = ("Ashleigh Barty", z1, 70, 80, 1, "Australia");
+		t3 = ("Stefanos Tsitsipas", z7, 55, 48, 3, "Grecia");
+		t4 = ("Karolina Pliskova", z5, 67, 68.5, 3, "Rep Checa");
+		t5 = ("Elina Svitolina", z6, 55.5, 68.5, 4, "Ucrania");
+		t6 = ("Alexander Zverev", z8, 59, 49.5, 4, "Alemania");
+		t7 = ("Aryna Sabalenka", z2, 67.5, 79.5, 2, "Bielorrusia");
+		t8 = ("Daniil Medvedev", z4, 79, 68, 2, "Rusia");
+                 */
+		//A continuación se inscribirán a los 8 tenistas en el campeonato
+    }
+}
---
+++ TennisDemo.java
@@ -0,0 +1,27 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Clase que lanza la simulación del campeonato de tenis
+ * 
+ * @author Equipo Docente DP 
+ * @version 1.0
+ */
+
+public class TennisDemo {
+
+    public static void main(String[] args) {
+        /*Creación del campeonato
+        Instanciar una variable de tipo Campeonato;
+        //Carga de datos iniciales del campeonato
+        Instanciar una variable de tipo InitTennisData pasando la variable instanciada de tipo campeonato como parámetro;
+        //Llamada al método que inicia la simulación del campeonato
+        Llamar al método que hayáis definido en vuestra clase Campeonato en la variable instanciada de tipo campeonato para que realice el campeonato;
+        */
+        
+    }
+}
